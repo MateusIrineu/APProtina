fde87d13b80b8c9c4d995eb918de00782c8c973b
Mateus Irineu <irineu.mateus11@gmail.com>
2025-08-08 20:30:27 -0300

    project

---
+++ HyouRotine/app/(tabs)/_layout.jsx
@@ -0,0 +1,10 @@
+import { Tabs } from "expo-router"
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+    // screenOptions={} estilização
+    ></Tabs>
+  )
+}
